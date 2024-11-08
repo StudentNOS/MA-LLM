a23ec0c46655b99167c5e202584028e6e54ed21b
eelisee <schrott.ew@t-online.de>
2024-11-08 22:00:55 +0100

    Add GPT_Selected_Titles.txt and update requirements; refactor API key handling in PATHS.py

--- PATHS.py
+++ PATHS.py
@@ -1,6 +1,10 @@
+import os
 
 #Hier den API Key eingeben
-api_key = ''
+api_key = os.getenv('OPENAI_API_KEY_NOS')
+
+if api_key is None:
+    raise ValueError("OPENAI_API_KEY environment variable is not set")
 
 #Hier die Enrez Mail eingeben
-email=''
+email='schrott.ew@t-online.de'
