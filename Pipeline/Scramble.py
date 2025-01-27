e1e21920df8350fe8e4248c334fc5f203d621969
Till Julius Adam <133876003+TillJAdam@users.noreply.github.com>
2025-01-27 05:32:10 +0100

    Add files via upload

---
+++ Pipeline/Scramble.py
@@ -0,0 +1,14 @@
+import random
+
+# Read numbers from the input file
+with open("Initial.txt", "r") as file:
+    numbers = file.read().splitlines()
+
+# Shuffle the numbers randomly
+random.shuffle(numbers)
+
+# Write the shuffled numbers to the output file
+with open("Initial.txt", "w") as file:
+    file.write("\n".join(numbers))
+
+print("Numbers have been shuffled and saved.")
